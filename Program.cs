abbec14bf71d8dad8735276119c00ef74b4ea11d
alexey-ryabkin <bumbarnaka@outlook.com>
2025-05-08 23:09:34 +0500

    Создал проект.
    Тестирую, правда ли я спрятал ключ API

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    internal class TelegramBot
+    {
+        static void Main(string[] args)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<TelegramBot>().Build();
+            Console.WriteLine(config["APIKey"]);
+        }
+    }
+}
